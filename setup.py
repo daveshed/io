69463af8639d1ad7da607df1a012948f90e18e6c
David Mohamad <david.mohamad@owlstone.co.uk>
2019-07-29 14:09:10 +0100

    Bump version (1.0.0) rename package

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
-    name="io",
-    version="0.0.0",
+    name="gpio",
+    version="1.0.0",
     packages=find_packages(),
     install_requires=[
         'stage',     # needs interface
